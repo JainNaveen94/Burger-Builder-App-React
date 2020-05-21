de4152f21338a7cecf7147ed23d7ee366738a6e9
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-21 23:18:04 +0530

    improvement changes done

--- src/Components/UI/Model/Model.js
+++ src/Components/UI/Model/Model.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 
 import modelCSS from "./Model.css";
 
 import Backdrop from "../Backdrop/Backdrop";
 
-const model = (props) => {
-  return (
-    <>
-    <Backdrop show={props.show} clicked={() => props.modelClose()} />
-    <div
-      className={modelCSS.Model}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-    </>
-  );
-};
 
-export default model;
+class Model extends Component {
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    return nextProps.show !== this.props.show;
+  }
+  
+  render() {
+    return (
+      <>
+      <Backdrop show={this.props.show} clicked={() => this.props.modelClose()} />
+      <div
+        className={modelCSS.Model}
+        style={{
+          transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: this.props.show ? "1" : "0",
+        }}
+      >
+        {this.props.children}
+      </div>
+      </>
+    );
+  }
+}
+
+// const model = (props) => {
+//   return (
+//     <>
+//     <Backdrop show={props.show} clicked={() => props.modelClose()} />
+//     <div
+//       className={modelCSS.Model}
+//       style={{
+//         transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+//         opacity: props.show ? "1" : "0",
+//       }}
+//     >
+//       {props.children}
+//     </div>
+//     </>
+//   );
+// };
+
+export default Model;
