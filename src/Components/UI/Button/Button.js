658d4334e2e264647e499f2e1e723ab671a12a90
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-23 05:03:09 +0530

    custom form with validation is done

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import buttonCSS from './Button.css';
+import buttonCSS from "./Button.css";
 
 const button = (props) => {
-    return (
-        <button className={[buttonCSS.Button, buttonCSS[props.btnType]].join(' ')} onClick={() => props.clicked()}>
-            {props.children}
-        </button>
-    )
-}
+  return (
+    <button
+      className={[buttonCSS.Button, buttonCSS[props.btnType]].join(" ")}
+      disabled={props.disabled}
+      onClick={() => props.clicked()}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default button;
