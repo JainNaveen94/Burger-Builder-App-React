f65b17aad06bc50b1cf44244f3a603406d1d1aec
Naveen Jain <47914199+JainNaveen94@users.noreply.github.com>
2020-05-22 18:53:17 +0530

    Basic Routing setup done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import appCSS from "./App.css";
 
 import Layout from "./hoc/Layout/Layout";
@@ -7,12 +9,18 @@ import BurgerBuilder from "./Containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./Containers/Checkout/Checkout";
 
 function App() {
-  return <div className={appCSS.App}>
-    <Layout>
-      <BurgerBuilder />
-      <Checkout />
-    </Layout>
-  </div>;
+  return (
+    <div className={appCSS.App}>
+      <Layout>
+        <Switch>
+          <Route path="/check-out" extact component={Checkout} />
+          <Route path="/" extact component={BurgerBuilder} />
+        </Switch>
+        {/* <BurgerBuilder />
+      <Checkout /> */}
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
